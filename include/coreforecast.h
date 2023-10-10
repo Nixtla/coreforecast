d1ecce430a2b92801b1c57a5cb848bc0de369ba4
José Morales <jmoralz92@gmail.com>
2023-10-10 13:21:55 -0600

    modify export

--- include/coreforecast.h
+++ include/coreforecast.h
@@ -2,25 +2,34 @@
 
 #include <cstdint>
 
+#ifdef _MSVC_VER
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+#define C_EXPORT extern "C" EXPORT
+
 typedef void *GroupedArrayHandle;
 
-extern "C" {
-int GroupedArray_CreateFromArrays(float *data, int32_t n_data, int32_t *indptr,
-                                  int32_t n_groups, GroupedArrayHandle *out);
+C_EXPORT {
+  int GroupedArray_CreateFromArrays(float *data, int32_t n_data,
+                                    int32_t *indptr, int32_t n_groups,
+                                    GroupedArrayHandle *out);
 
-int GroupedArray_Delete(GroupedArrayHandle handle);
+  int GroupedArray_Delete(GroupedArrayHandle handle);
 
-int GroupedArray_MinMaxScalerStats(GroupedArrayHandle handle, double *out);
+  int GroupedArray_MinMaxScalerStats(GroupedArrayHandle handle, double *out);
 
-int GroupedArray_StandardScalerStats(GroupedArrayHandle handle, double *out);
+  int GroupedArray_StandardScalerStats(GroupedArrayHandle handle, double *out);
 
-int GroupedArray_RobustScalerIqrStats(GroupedArrayHandle handle, double *out);
+  int GroupedArray_RobustScalerIqrStats(GroupedArrayHandle handle, double *out);
 
-int GroupedArray_RobustScalerMadStats(GroupedArrayHandle handle, double *out);
+  int GroupedArray_RobustScalerMadStats(GroupedArrayHandle handle, double *out);
 
-int GroupedArray_ScalerTransform(GroupedArrayHandle handle, double *stats,
-                                 float *out);
+  int GroupedArray_ScalerTransform(GroupedArrayHandle handle, double *stats,
+                                   float *out);
 
-int GroupedArray_ScalerInverseTransform(GroupedArrayHandle handle,
-                                        double *stats, float *out);
+  int GroupedArray_ScalerInverseTransform(GroupedArrayHandle handle,
+                                          double *stats, float *out);
 }
