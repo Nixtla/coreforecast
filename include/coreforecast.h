38eeeb8e2527febeb8a6e3e8aee4164dc8f4d1f1
José Morales <jmoralz92@gmail.com>
2023-10-10 13:58:55 -0600

    prefix dll export and fix warnings

--- include/coreforecast.h
+++ include/coreforecast.h
@@ -8,28 +8,30 @@
 #define DLL_EXPORT
 #endif
 
-#define C_EXPORT extern "C" DLL_EXPORT
-
 typedef void *GroupedArrayHandle;
 
-C_EXPORT {
-  int GroupedArray_CreateFromArrays(float *data, int32_t n_data,
-                                    int32_t *indptr, int32_t n_groups,
-                                    GroupedArrayHandle *out);
+extern "C" {
+DLL_EXPORT int GroupedArray_CreateFromArrays(float *data, int32_t n_data,
+                                             int32_t *indptr, int32_t n_groups,
+                                             GroupedArrayHandle *out);
 
-  int GroupedArray_Delete(GroupedArrayHandle handle);
+DLL_EXPORT int GroupedArray_Delete(GroupedArrayHandle handle);
 
-  int GroupedArray_MinMaxScalerStats(GroupedArrayHandle handle, double *out);
+DLL_EXPORT int GroupedArray_MinMaxScalerStats(GroupedArrayHandle handle,
+                                              double *out);
 
-  int GroupedArray_StandardScalerStats(GroupedArrayHandle handle, double *out);
+DLL_EXPORT int GroupedArray_StandardScalerStats(GroupedArrayHandle handle,
+                                                double *out);
 
-  int GroupedArray_RobustScalerIqrStats(GroupedArrayHandle handle, double *out);
+DLL_EXPORT int GroupedArray_RobustScalerIqrStats(GroupedArrayHandle handle,
+                                                 double *out);
 
-  int GroupedArray_RobustScalerMadStats(GroupedArrayHandle handle, double *out);
+DLL_EXPORT int GroupedArray_RobustScalerMadStats(GroupedArrayHandle handle,
+                                                 double *out);
 
-  int GroupedArray_ScalerTransform(GroupedArrayHandle handle, double *stats,
-                                   float *out);
+DLL_EXPORT int GroupedArray_ScalerTransform(GroupedArrayHandle handle,
+                                            double *stats, float *out);
 
-  int GroupedArray_ScalerInverseTransform(GroupedArrayHandle handle,
-                                          double *stats, float *out);
+DLL_EXPORT int GroupedArray_ScalerInverseTransform(GroupedArrayHandle handle,
+                                                   double *stats, float *out);
 }
