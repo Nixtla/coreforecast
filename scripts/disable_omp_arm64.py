98fe593195b886f69d307c6514397fe740c0cde4
José Morales <jmoralz92@gmail.com>
2023-11-01 16:34:22 -0600

    use toml

--- scripts/disable_omp_arm64.py
+++ scripts/disable_omp_arm64.py
@@ -1,11 +1,11 @@
 import os
 
-import tomli
+import toml
 
 
 if os.environ["CIBW_BUILD"] == "cp38-macosx_arm64":
     with open("pyproject.toml", "rt") as f:
-        cfg = tomli.load(f)
+        cfg = toml.load(f)
     cfg["tool"]["scikit-build"]["cmake"]["args"] = ["-DUSE_OPENMP=OFF"]
     with open("pyproject.toml", "wt") as f:
-        tomli.dump(cfg, f)
+        toml.dump(cfg, f)
