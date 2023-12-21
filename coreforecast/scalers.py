6a7acb6a5da6ba59062e24d917561fe699a85e5b
José Morales <jmoralz92@gmail.com>
2023-12-20 18:04:30 -0600

    add docs (#11)

--- coreforecast/scalers.py
+++ coreforecast/scalers.py
@@ -3,31 +3,70 @@ import numpy as np
 from .grouped_array import GroupedArray
 
 
-class BaseLocalScaler:
-    scaler_type: str
+__all__ = [
+    "LocalMinMaxScaler",
+    "LocalStandardScaler",
+    "LocalRobustScaler",
+]
 
-    def fit(self, ga: GroupedArray) -> "BaseLocalScaler":
-        self.stats_ = ga.scaler_fit(self.scaler_type)
+
+class _BaseLocalScaler:
+    _scaler_type: str
+
+    def fit(self, ga: GroupedArray) -> "_BaseLocalScaler":
+        """Compute the statistics for each group.
+
+        Args:
+            ga (GroupedArray): Array with grouped data.
+
+        Returns:
+            self: The fitted scaler object."""
+        self.stats_ = ga._scaler_fit(self._scaler_type)
         return self
 
     def transform(self, ga: GroupedArray) -> np.ndarray:
-        return ga.scaler_transform(self.stats_)
+        """Use the computed statistics to apply the transformation.
+
+        Args:
+            ga (GroupedArray): Array with grouped data.
+
+        Returns:
+            np.ndarray: Array with the transformed data."""
+        return ga._scaler_transform(self.stats_)
 
     def inverse_transform(self, ga: GroupedArray) -> np.ndarray:
-        return ga.scaler_inverse_transform(self.stats_)
+        """Use the computed statistics to invert the transformation.
+
+        Args:
+            ga (GroupedArray): Array with grouped data.
+
+        Returns:
+            np.ndarray: Array with the inverted transformation."""
+        return ga._scaler_inverse_transform(self.stats_)
 
 
-class LocalMinMaxScaler(BaseLocalScaler):
-    scaler_type = "MinMax"
+class LocalMinMaxScaler(_BaseLocalScaler):
+    """Scale each group to the [0, 1] interval"""
+
+    _scaler_type = "MinMax"
 
 
-class LocalStandardScaler(BaseLocalScaler):
-    scaler_type = "Standard"
+class LocalStandardScaler(_BaseLocalScaler):
+    """Scale each group to have zero mean and unit variance"""
+
+    _scaler_type = "Standard"
 
 
-class LocalRobustScaler(BaseLocalScaler):
+class LocalRobustScaler(_BaseLocalScaler):
+    """Scale each group using robust statistics
+
+    Args:
+        scale (str): Type of robust scaling to use. Valid options are 'iqr' and 'mad'.
+            If 'iqr' will use the inter quartile range as the scale.
+            If 'mad' will use median absolute deviation as the scale."""
+
     def __init__(self, scale: str):
         if scale == "iqr":
-            self.scaler_type = "RobustIqr"
+            self._scaler_type = "RobustIqr"
         else:
-            self.scaler_type = "RobustMad"
+            self._scaler_type = "RobustMad"
