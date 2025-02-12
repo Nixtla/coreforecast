08eba8538d6e2369ff7e0b3a37ecf67e32aa2a99
Christopher Titchen <109701765+christophertitchen@users.noreply.github.com>
2025-02-12 19:14:08 +0000

    fix: add non-contiguous array support for EMA

--- include/coreforecast/exponentially_weighted.h
+++ include/coreforecast/exponentially_weighted.h
@@ -2,10 +2,10 @@
 
 namespace exponentially_weighted {
 template <typename T>
-inline void MeanTransform(const T *data, int n, T *out, T alpha) {
+inline void MeanTransform(const T *data, int n, T *out, T alpha, ptrdiff_t stride) {
   out[0] = data[0];
   for (int i = 1; i < n; ++i) {
-    out[i] = alpha * data[i] + (1 - alpha) * out[i - 1];
+    out[i] = alpha * data[i * stride / sizeof(T)] + (1 - alpha) * out[i - 1];
   }
 }
 } // namespace exponentially_weighted
--- src/exponentially_weighted.cpp
+++ src/exponentially_weighted.cpp
@@ -6,7 +6,8 @@ template <typename T>
 py::array_t<T> ExponentiallyWeightedMean(const py::array_t<T> data, T alpha) {
   py::array_t<T> out(data.size());
   exponentially_weighted::MeanTransform<T>(data.data(), data.size(),
-                                           out.mutable_data(), alpha);
+                                           out.mutable_data(), alpha, 
+                                           data.strides()[0]);
   return out;
 }
 
