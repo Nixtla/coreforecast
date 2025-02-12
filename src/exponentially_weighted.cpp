4f44bf3a427905934c0e8defe6833381a9be9a4e
Christopher Titchen <109701765+christophertitchen@users.noreply.github.com>
2025-02-12 23:56:54 +0000

    fix: resolve linting error

--- src/exponentially_weighted.cpp
+++ src/exponentially_weighted.cpp
@@ -3,7 +3,9 @@
 #include "exponentially_weighted.h"
 
 template <typename T>
-py::array_t<T> ExponentiallyWeightedMean(const py::array_t<T, py::array::c_style | py::array::forcecast> data, T alpha) {
+py::array_t<T> ExponentiallyWeightedMean(
+    const py::array_t<T, py::array::c_style | py::array::forcecast> data,
+    T alpha) {
   py::array_t<T> out(data.size());
   exponentially_weighted::MeanTransform<T>(data.data(), data.size(),
                                            out.mutable_data(), alpha);
