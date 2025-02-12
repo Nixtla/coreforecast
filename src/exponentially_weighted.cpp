60069cd0cd3f85992ac67a448e4a920ade94062d
Christopher Titchen <109701765+christophertitchen@users.noreply.github.com>
2025-02-12 23:45:12 +0000

    fix: simplify bug fix

--- src/exponentially_weighted.cpp
+++ src/exponentially_weighted.cpp
@@ -3,11 +3,10 @@
 #include "exponentially_weighted.h"
 
 template <typename T>
-py::array_t<T> ExponentiallyWeightedMean(const py::array_t<T> data, T alpha) {
+py::array_t<T> ExponentiallyWeightedMean(const py::array_t<T, py::array::c_style | py::array::forcecast> data, T alpha) {
   py::array_t<T> out(data.size());
   exponentially_weighted::MeanTransform<T>(data.data(), data.size(),
-                                           out.mutable_data(), alpha, 
-                                           data.strides()[0]);
+                                           out.mutable_data(), alpha);
   return out;
 }
 
